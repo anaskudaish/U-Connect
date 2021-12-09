203104107d9bae6cbdfd35f1dff14ac9256347ae
derfloh42 <38074020+derfloh42@users.noreply.github.com>
2021-12-09 10:48:02 +0100

    Update passwort_zuruecksetzen.blade.php

--- views/Passwort_Zuruecksetzen/passwort_zuruecksetzen.blade.php
+++ views/Passwort_Zuruecksetzen/passwort_zuruecksetzen.blade.php
@@ -5,11 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <title>Passwort zurücksetzen</title>
-    <link rel="stylesheet" href="../css/Passwort_zuruecksetzen.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/anmeldung.css">
 </head>
 
 <body>
-
+<div class="background" >
+    <div class="shape"></div>
+    <div class="shape"></div>
+</div>
 <div class="container">
 
     <form class="form"  method="POST" action="/passwort_zuruecksetzen_verifizierung">
