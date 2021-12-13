25dfeca71b3187afb8d2cfa5acf64c079e0bef9b
derfloh42 <38074020+derfloh42@users.noreply.github.com>
2021-12-13 20:23:32 +0100

    Add files via upload

---
+++ views/Events/Event_bearbeiten.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Event Bearbeiten</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/logo-swe.css">
+    <link rel="stylesheet"  href="../css/Events_planen.css">
+    <link rel="stylesheet"  href="../css/neues_event.css">
+</head>
+<img src="../img/logo-swe.png" class="sweLogo" alt="sweLogo">
+<body>
+<a href="/Events_planen" class="previous">&laquo;Events planen</a>
+<div class="container">
+    <div class="main-body">
+        <form id="contact" action="/event_hinzufuegen" method="post" enctype="multipart/form-data">
+            <h3>Event Bearbeiten</h3>
+            <div class="form__input-error-message">@if(isset($resultatfehler)){{$resultatfehler}} @endif</div>
+            <div class="form__message form__message--success">@if(isset($resultatok)){{$resultatok}} @endif</div>
+            <fieldset>
+                <input  type="text" name="Eventname" required autofocus value="{{$eventData['eventname']}}">
+            </fieldset>
+            <fieldset>
+                <label for="date"> Datum: </label>
+                <input id="date" type="date" name="date" value="{{$eventData['Datum']}}">
+            </fieldset>
+            <fieldset>
+                <label for="time"> Uhrzeit: </label>
+                <input id ="time"    type="time" name="time" value="{{$eventData['Uhrzeit']}}">
+            </fieldset>
+            <fieldset>
+                <button name="submit" type="submit">Event speichern</button>
+            </fieldset>
+            <input type="hidden" name="submitted" value="1" >
+
+            <label>Teilnehmer Liste:</label>
+            <div id="teilnehmer">
+                @if(!empty($userList))
+                    <select name="test" size="10">
+                    @foreach($userList as $value)
+                        <option id="kontaktInEvent">{{$value['vorname'] }}</option>
+                    @endforeach
+                    </select>
+                @endif
+            </div>
+            <button name="submit" type="submit">Neuen Teilnehmer hinzufügen</button>
+            <button name="submit" type="submit">Ausgewählten Teilnehmer entfernen</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
---
+++ views/Events/Events_planen.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Events planen</title>
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../css/Events_planen.css">
+    <link rel="stylesheet" href="../css/logo-swe.css">
+
+
+
+</head>
+<body>
+<img src="../img/logo-swe.png" class="sweLogo" alt="sweLogo">
+<a href="/" class="previous">&laquo;Hauptseite</a>
+
+
+    <div class="container">
+        <div class="main-body">
+                <div class="col-lg-8">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Anstehende Events:</h6>
+                                </div>
+                            @if(!empty($events))
+                                    <select name="test" size="10">
+                                    @foreach($events as $value)
+                                            <option value="{{$value['EventName'] }}">{{$value['EventName'] }}</option>
+                                    @endforeach
+                                    </select>
+                            @endif
+                        </div>
+                            <br>
+
+
+                            <div class="row mb-3">
+                                <div class="col-sm-9 text-secondary">
+                                    <form  method="post" action="/neues_Event" >
+                                    <input   type="submit" name="submit" class="btn btn-primary px-4" value="Neues Event planen">
+                                    </form>
+                                </div>
+                            </div>
+
+
+
+                                <div class="row mb-3">
+                                <div class="col-sm-9 text-secondary">
+                                        <form  method="post" action="/Event_bearbeiten" >
+                                        <input  type="submit" name="submit" class="btn btn-primary px-4" value="Ausgewähltes Event bearbeiten">
+                                        <input type="hidden" name="id_kontakt" value="{{$Events['id']}}">
+                                        <input type="hidden" name="beziehungen" value="1" >
+                                        </form>
+                                </div>
+                                </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-9 text-secondary">
+                                    <form  method="post" action="/Events_planen" >
+                                        <input  type="submit" name="submit" class="btn btn-primary px-4" value="Ausgewähltes Event löschen">
+                                        <input type="hidden" name="id_kontakt" value="{{$Events['id']}}">
+                                        <input type="hidden" name="beziehungen" value="1" >
+                                    </form>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
