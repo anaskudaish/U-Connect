7abb8696607231d2047059734b4f6912b67d2119
derfloh42 <38074020+derfloh42@users.noreply.github.com>
2022-01-10 13:21:41 +0100

    Update EventController.php

--- controllers/EventController.php
+++ controllers/EventController.php
@@ -1,6 +1,5 @@
 <?php
 require_once('../models/eventsListe.php');
-//require_once('../models/kontaktenzeigen.php');
 
 class EventController
 {
@@ -68,10 +67,11 @@ class EventController
             $eventData = ausgewaehtlesevent($_POST['eventId']);
             $userList = teilnehmerDesEvents($_POST['eventId']);
             $andereKontakte= nichtteilnehmerDesEvents($_POST['eventId'],$_SESSION['email']);
-
+            $besteKontakte = besteTeilnehmer($_POST['eventId']);
             $daten = ['eventData' => $eventData,
                 'userList' => $userList,
-                'andereKontakte' => $andereKontakte
+                'andereKontakte' => $andereKontakte,
+                'besteKontakte' => $besteKontakte
             ];
             return view('Events.Teilnehmer_Hinzufuegen',$daten);
         }else{
@@ -102,8 +102,9 @@ class EventController
             $andereKontakte=null;
             $eventData = ausgewaehtlesevent($_POST['eventId']);
             $userList = teilnehmerDesEvents($_POST['eventId']);
+            $besteKontakte = besteTeilnehmer($_POST['eventId']);
             if($search_text==""){
-                //$andereKontakte= nichtteilnehmerDesEvents($_POST['eventId'],$_SESSION['email']);
+                $andereKontakte= nichtteilnehmerDesEvents($_POST['eventId'],$_SESSION['email']);
                 $daten = ['eventData' => $eventData,
                     'userList' => $userList,
                     'andereKontakte' => $andereKontakte
@@ -119,9 +120,12 @@ class EventController
                     $andereKontakte = Suche_NichtTeilnehmer($email,$_POST['eventId'],"tags",$search_text);
                 }
             }
+            $FilteredbesteKontakte = getMatching($besteKontakte,$andereKontakte);
+            var_dump($andereKontakte);
             $daten = ['eventData' => $eventData,
                 'userList' => $userList,
-                'andereKontakte' => $andereKontakte
+                'andereKontakte' => $andereKontakte,
+                'besteKontakte' => $FilteredbesteKontakte
             ];
         }
         return view('Events.Teilnehmer_Hinzufuegen',$daten);
@@ -140,11 +144,13 @@ class EventController
             $andereKontakte = nichtteilnehmerDesEvents($eventID,$_SESSION['email']);
             $ausgewaehtlerKontakt = getKontakt($KontaktID);
             $KontaktBeziehung = getBeziehungenImEvent($eventID,$KontaktID,$isEmpty);
+            $besteKontakte = besteTeilnehmer($eventID);
             $daten = ['eventData' => $eventData,
                 'userList' => $userList,
                 'andereKontakte' => $andereKontakte,
                 'KontaktBeziehung' => $KontaktBeziehung,
-                'ausgewaehtlerKontakt' => $ausgewaehtlerKontakt
+                'ausgewaehtlerKontakt' => $ausgewaehtlerKontakt,
+                'besteKontakte' => $besteKontakte
             ];
             return view('Events.Teilnehmer_Hinzufuegen',$daten);
         }else{
@@ -155,16 +161,17 @@ class EventController
     public function Ausgewaehlten_Kontakt_Hinzufuegen(){
         if (isset($_SESSION['login_ok']) && $_SESSION['login_ok'] == 1) {
             $eventID = $_POST['eventId'];
+            if(!IstTeilnehmer($eventID,$_POST['kontaktID'])){
             teilnehmerHinzufuegen($eventID,$_POST['kontaktID']);
-            echo $eventID."<br>";
-            echo $_POST['kontaktID'];
+            }
             $eventData = ausgewaehtlesevent($eventID);
             $userList = teilnehmerDesEvents($eventID);
             $andereKontakte= nichtteilnehmerDesEvents($eventID,$_SESSION['email']);
-
+            $besteKontakte = besteTeilnehmer($eventID);
             $daten = ['eventData' => $eventData,
                 'userList' => $userList,
                 'andereKontakte' => $andereKontakte,
+                'besteKontakte' => $besteKontakte
             ];
             return view('Events.Teilnehmer_Hinzufuegen',$daten);
         }else{
