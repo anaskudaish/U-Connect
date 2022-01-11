e11fad469a33c98c84ff64afd56012cdedfec0cc
DerGiftige <94870456+DerGiftige@users.noreply.github.com>
2022-01-11 19:43:10 +0100

    table für benutzerdefinierte urls

---
+++ sql.sql
@@ -0,0 +1,121 @@
+CREATE SCHEMA adressbuch COLLATE 'utf8_unicode_ci';
+USE adressbuch;
+
+
+create table nutzer(
+
+                       id int(10) PRIMARY KEY not null auto_increment,
+                       email varchar(100) unique not null,
+                       passwort varchar(200) not null ,
+                       passwort_zurueck int(1) default 0
+
+);
+
+create table temporaeres_passwort(
+                                     id int(10)  primary key not null references nutzer(id)
+                                         ON DELETE CASCADE,
+                                     passwort varchar(200) not null,
+                                     ablauf_zeit varchar(80) not null
+
+
+);
+
+
+
+create table nutzer_tags(
+                            id int(10) primary key  not null references nutzer(id)
+                                ON DELETE CASCADE,
+                              tags varchar(800) not null
+);
+
+create table kontakte(
+                         id int(10) auto_increment primary key not null,
+                         nutzer_id int(10) not null references nutzer(id)
+                             ON DELETE CASCADE,
+                         vorname varchar(80)  not null ,
+                         nachname varchar(80) not null ,
+                         bildname varchar(200) not null ,
+                         erinnerungsinterval varchar(80) not null ,
+                         naechsteerinnerung  varchar(80) not null
+
+);
+
+create table telefonnummer_kontakte(
+
+                                       id int(10)  primary key not null references kontakte(id)
+                                           ON DELETE CASCADE,
+                                       telefonnummer varchar(80) not null
+);
+
+create table text_kontakte(
+                              id int(10) primary key not null references kontakte(id)
+                                  ON DELETE CASCADE,
+                           textfeld varchar(800) not null
+
+);
+
+create table adresse_kontakte(
+
+                                 id int(10)  primary key not null references kontakte(id)
+                                     ON DELETE CASCADE,
+                                 strasse varchar(80) default null,
+                                 plz varchar(80) default null,
+                                 stadt varchar(80) default null ,
+                                 land varchar(80) default null
+);
+
+create table socialMedia_Kontakte(
+                                     id int(10)  primary key not null references kontakte(id)
+                                         ON DELETE CASCADE,
+                                     instagram varchar(300) default null,
+                                     facebook varchar (300) default null,
+                                     twitter varchar(300) default null
+
+
+);
+
+
+create table tags_kontakte(
+                              id int(10) not null references kontakte(id)
+                                  ON DELETE CASCADE,
+                              tags varchar(800) not null,
+                              primary key (id,tags)
+
+);
+
+create table geburtsdatum_kontakte(
+                              id int(10) primary key not null references kontakte(id)
+                                  ON DELETE CASCADE,
+                              geburtsdatum varchar(80) not null
+
+);
+
+CREATE TABLE beziehungen_kontakte(
+                                     id int(10) not null REFERENCES kontakte(id)
+                                         ON DELETE CASCADE,
+                                     id_beziehung int(10) not null,
+                                     Beziehungs_wert int(10),
+                                     PRIMARY KEY (id,id_beziehung)
+);
+
+create table events(
+                         id int(10) auto_increment primary key not null,
+                         nutzer_id int(10) not null references nutzer(id)
+                             ON DELETE CASCADE,
+                         eventname varchar(80)  not null ,
+                         Datum varchar(80) not null ,
+                         Uhrzeit varchar(200) not null
+);
+
+create table event_kontakte(
+    event_id int not null references events(id) ON DELETE CASCADE,
+    kontakte_id int not null references kontakte(id) ON DELETE CASCADE
+); 
+
+create table urls_kontakte(
+                              id int(10) not null references kontakte(id)
+                                  ON DELETE CASCADE,
+                              url varchar(800) not null,
+                              primary key (id,url)
+
+);
